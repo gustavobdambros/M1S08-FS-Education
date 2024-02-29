2a275b8754ddeeb1d7cec4a51a0ec2f8edbdacc8
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-02-29 06:57:38 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fullstack/M1S08/M1S08Application.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.M1S08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1S08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1S08Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fullstack/M1S08/M1S08ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fullstack.M1S08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1S08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
