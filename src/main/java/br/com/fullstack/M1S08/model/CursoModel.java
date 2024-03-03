82a90cefe35f8096fb491a55bfa31c4340b8da17
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-03-02 23:08:33 -0300

    [M1S08] Ex 5 - Service: Cursos - Criação de CursoService

--- src/main/java/br/com/fullstack/M1S08/model/CursoModel.java
+++ src/main/java/br/com/fullstack/M1S08/model/CursoModel.java
@@ -2,13 +2,18 @@ package br.com.fullstack.M1S08.model;
 
 import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 public class CursoModel {
 
     // Início da contagem de ID's
     private static Integer proximoId = 1;
+    @Getter private static List<CursoModel> cursos = new ArrayList<>();
 
     // Atributos
     @Setter(AccessLevel.NONE) private Integer id;
@@ -17,7 +22,13 @@ public class CursoModel {
     private Integer cargaHoraria;
 
     // Método para controlar a sequência de ID's
-    private Integer getProximoId(){
+    private static Integer getProximoId(){
         return proximoId++;
+    }
+
+    public static CursoModel inserir(CursoModel curso) {
+        curso.id = getProximoId();
+        cursos.add(curso);
+        return curso;
     }
 }
